14ddc231dba55cba33fea3bbfac25ee6a57688fd
AxelDuval <axel.duval@gmail.com>
2021-10-09 17:09:30 +0200

    Create global variables and checking function

---
+++ README.md
@@ -0,0 +1,3 @@
+******************** Quizz *************************
+
+Projet de création d'un quizz dans le cadre de l'apprentissage du JavaScript
--- js/main.js
+++ js/main.js
@@ -1,8 +1,35 @@
 const form = document.querySelector(".form-quizz");
 let arrayResults = [];
+const solutions = ["c", "a", "b", "a", "c"];
+const emojis = ['✔️','✨','👀','😭','👎'];
+const titleResult = document.querySelector(".resultats h2");
+const textResult = document.querySelector(".note");
+const helpResult = document.querySelector(".aide");
+const allQuestions = document.querySelectorAll(".question-block");
+let checkArray = [];
+
+
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
+    // console.log(document.querySelector("input[name='q1']:checked").value);
 
-    console.log(document.querySelector("input[name='q1']:checked").value);
+    for(i=1; i<6; i++) {
+        arrayResults.push(document.querySelector(`input[name='q${i}']:checked`).value);
+    }
+    console.log(arrayResults);
+    checkFunction(arrayResults);
+    arrayResults = [];
 })
+
+function checkFunction(arrayResults) {
+    for(let a=0; a < 5; a++){
+        if(arrayResults[a] === solutions[a]){
+            checkArray.push(true);
+        }
+        else {
+            checkArray.push(false);
+        }
+        console.log(checkArray);
+    }
+}
